221aeec8218f8943b18496829504ad9152b15d22
frank <diaowangzhen@163.com>
2018-12-30 17:58:44 +0800

    简单实现一个类Java的异常处理机制

--- chapterOne/exception.php
+++ chapterOne/exception.php
@@ -1,1 +1,33 @@
 <?php
+
+/**
+ * @param $errno int 错误级别
+ * @param $errstr string 错误信息
+ * @param $errfile string 错误文件
+ * @param $errline int 错误行号
+ * @throws Exception
+ */
+function error_handler($errno,$errstr,$errfile,$errline){
+    throw new Exception($errstr);
+}
+
+set_error_handler("error_handler");
+
+class test {
+    public function main($num){
+        $n = 0;
+        try{
+            $n = $num/0;
+        }catch (Exception $e){
+            print_r($e->getFile());
+            print_r($e->getLine());
+            print_r($e->getMessage());
+            die;
+        }
+
+        return $n;
+    }
+}
+
+$t = new test();
+echo $t->main(2);
